e126cdff7870cc00cf0cfdc94092fc5cf25a1ba1
Aftab Khan <aftabnack@gmail.com>
2022-08-12 23:46:17 +0530

    Fixing asset paths in prod

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,26 @@ import Resume from "./resume";
 
 Font.register({
   family: "Mouse Memoirs",
-  src: "/fonts/MouseMemoirs-Regular.ttf",
+  src: import.meta.env.BASE_URL + "fonts/MouseMemoirs-Regular.ttf",
 });
 Font.register({
   family: "Lato",
   fonts: [
-    { src: import.meta.env.BASE_URL + `fonts/Lato-Regular.ttf` }, // font-style: normal, font-weight: normal
+    { src: import.meta.env.BASE_URL + "fonts/Lato-Regular.ttf" }, // font-style: normal, font-weight: normal
     {
-      src: import.meta.env.BASE_URL + `fonts/Lato-Italic.ttf`,
+      src: import.meta.env.BASE_URL + "fonts/Lato-Italic.ttf",
       fontStyle: "italic",
     },
     {
-      src: import.meta.env.BASE_URL + `fonts/Lato-Light.ttf`,
+      src: import.meta.env.BASE_URL + "fonts/Lato-Light.ttf",
       fontWeight: 300,
     },
     {
-      src: import.meta.env.BASE_URL + `fonts/Lato-LightItalic.ttf`,
+      src: import.meta.env.BASE_URL + "fonts/Lato-LightItalic.ttf",
       fontWeight: 300,
       fontStyle: "italic",
     },
-    { src: import.meta.env.BASE_URL + `fonts/Lato-Bold.ttf`, fontWeight: 700 },
+    { src: import.meta.env.BASE_URL + "fonts/Lato-Bold.ttf", fontWeight: 700 },
   ],
 });
 Font.registerHyphenationCallback((word) => [word]);
