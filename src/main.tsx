baaea6ea383e276989a20f9eca41cef8bddee328
Aftab Khan <aftabnack@gmail.com>
2022-08-12 18:07:49 +0530

    Had to add some BS amount of plugins & packages for the rendering to work correctly. Apparently we are mimicing nodejs functions for browser

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactPDF, { PDFViewer } from "@react-pdf/renderer";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PDFViewer>
+      <App />
+    </PDFViewer>
   </React.StrictMode>
-)
+);
